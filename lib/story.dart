9e29f1d09a44231c58298ff9236de7a936365583
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-11-25 22:04:55 +0000

    text for choice2 does not show!

---
+++ lib/story.dart
@@ -0,0 +1,35 @@
+//TODO: Step 2 - Create a new class called Story.
+class Story {
+//TODO: Step 3 - Create 3 properties for this class,
+/* A. storyTitle to store the story text.
+ B. choice1 to store the text for choice 1,
+ C. choice2 to store the text for choice 2.
+ */
+  String storyTitle;
+  String choice1;
+  String choice2;
+//TODO: Step 4 - Create a Constructor for this class to be able
+// to initialise the properties created in step 3.
+  // Story Story({   required String storyTitle,   required String choice1,   required String choice2, })
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
+
+/*
+# destiniflutter
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+ */
